fd9d706d47fc92f385ebbcd07b433b160e786e85
yuji-matsunami <yuji.sohuto@gmail.com>
2021-12-26 23:53:30 +0900

    update read praises

--- static/js/displayPraises.js
+++ static/js/displayPraises.js
@@ -14,9 +14,19 @@ for (var c of cookiesArray) {
 console.log(comment);
 var commentArray = comment.split(',')
 
-var commentList = []
-for (var i=0; i<commentArray.length; i++) {
-    commentList.push('<li>'+ user_name +commentArray[i] + '</li>');
+// var commentList = []
+// for (var i=0; i<commentArray.length; i++) {
+//     commentList.push('<li>'+ user_name +commentArray[i] + '</li>');
+// }
+
+var heart_comment = document.getElementById("heart-comment")
+for (let i=0; i < 10; i++) {
+    if (i < commentArray.length) {
+        heart_comment.insertAdjacentHTML('afterbegin', '<div class="comment"><li class="heart"><img src="/static/images/heart_pink.png" width="350px" height="350px"><p>'+user_name+commentArray[i]+'</p></li></div>')
+    }
+    else {
+        heart_comment.insertAdjacentHTML('afterbegin', '<div class="comment"><li class="heart"><img src="/static/images/heart_pink.png" width="350px" height="350px"></li></div>')
+    }
 }
 console.log(commentList)
 document.getElementById("comment-list").innerHTML = commentList.join('');
