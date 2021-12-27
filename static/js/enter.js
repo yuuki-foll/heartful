bc57a642a25a743180fdea8e12a998118c8155d1
yuji-matsunami <yuji.sohuto@gmail.com>
2021-12-27 12:20:22 +0900

    add enter btn animation

--- static/js/enter.js
+++ static/js/enter.js
@@ -6,6 +6,14 @@ name_btn.addEventListener('click', () => {
     const output = "ようこそ!"+ user_name +"さん";
     document.cookie = "name="+user_name;
     document.getElementById("output-name").innerText = output;
-    document.getElementById("link1").innerText = "褒めたい人はこちら";
-    document.getElementById("link2").innerText = "褒められたい人はこちら";
+    document.getElementById("link1").innerHTML = '<a id="link1-btn" class="link-btn">褒めたい人はこちら</a>';
+    document.getElementById("link2").innerHTML = '<a id="link2-btn" class="link-btn">褒められたい人はこちら</a>';
+    add_url();
 });
+
+function add_url() {
+    const link1 = document.getElementById("link1-btn");
+    link1.href = "http://127.0.0.1:8080/praise";
+    const link2 = document.getElementById("link2-btn");
+    link2.href = "http://127.0.0.1:8080/user_info";
+}
